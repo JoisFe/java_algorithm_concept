4907b3779e81209c8ea2c070e513a5880b9a206e
조재철 <rhsnqk@gmail.com>
2022-10-04 23:13:24 +0900

    union-find 구현

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,15 @@ G
 src/sort/quickSort.java,3/4/345d3b23b79d8bcbb6e6ece0f9b4a2837798e9cb
 H
 src/sort/BubbleSort.java,c/4/c4ca745fbe1b508a29753bfe745877aa88157193
+C
+src/search/Dfs.java,d/2/d2ce15aa53bcefe2818c08f1044b64975d0453c6
+N
+src/shortestpath/Dijkstra.java,9/8/984e7f41488baa672c80586914365fa8a9f24346
+B
+src/sort/Prac.java,a/2/a21584711a678792aabb77b65b30760356d5da19
+K
+src/sort/InsertionSort.java,d/5/d5cc399d32304f5b96d7ad48dfe5a26ac3f97592
+D
+src/search/Prac.java,1/d/1d5c41f1171d9d2889713395180e40f17d6f4e1d
+C
+src/search/Bfs.java,a/7/a75a9918842aafd65ce47a01135b895685231b37
---
+++ src/othergraph/UnionFind.java
@@ -0,0 +1,70 @@
+package othergraph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+public class UnionFind {
+    protected static int v;
+    protected static int e;
+    protected static List<Integer> parent = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        v = scanner.nextInt();
+        e = scanner.nextInt();
+
+        for (int i = 0; i <= v; ++i) {
+            parent.add(i);
+        }
+
+        for (int i = 0; i < e; ++i) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            unionParent(a, b);
+        }
+
+        System.out.println("각 원소가 속한 집합: ");
+
+        for (int i = 1; i <= v; ++i) {
+            System.out.print(findParent(i) + " ");
+        }
+        System.out.println();
+
+        System.out.println("부모 테이블: ");
+
+        for (int i = 1; i <= v; ++i) {
+            System.out.print(parent.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    private static void unionParent(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+
+        if (a < b) {
+            parent.set(b, a);
+        } else {
+            parent.set(a, b);
+        }
+
+    }
+
+    private static int findParent(int a) {
+        if (a == parent.get(a)) {
+            return a;
+        }
+
+        int parent = findParent(UnionFind.parent.get(a));
+        UnionFind.parent.set(a, parent);
+
+        return parent;
+    }
+}
